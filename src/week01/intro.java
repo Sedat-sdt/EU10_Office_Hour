4a1b7fcf976ec122c418d52f4a368009b1290387
Sedat Maden <madensedat05@gmail.com>
2022-12-21 21:20:50 +0300

    Initial commit

---
+++ src/week01/intro.java
@@ -0,0 +1,63 @@
+package week01;
+
+public class intro {
+    public static void main(String[] args) {
+
+       // System.out.println("1\n2\n3\n123");
+
+        //System.out.println("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are");
+
+        //System.out.println("*****\n****\n***\n**\n*\n**\n***\n****\n*****");
+
+
+        System.out.println("-----------------\n" +
+                          "|Date: 19.10.2022|\n" +
+                          "|Table No.:    06|\n" +
+                          "|                |\n" +
+                          "|Item       Price|\n" +
+                          "|Water          2|\n" +
+                          "|Coffee        12|\n" +
+                          "|Coffee        12|\n" +
+                          "|Tea            8|\n" +
+                          "|Total         22|\n" +
+                          "-----------------");
+/*
+Date: 19.10.2022|
+|Table No.:    06|
+|                |
+|Item       Price|
+|Water          2|
+|Coffee        12|
+|Tea            8|
+|Total         22|
+-----------------
+   /*
+
+    10
+   / \
+  20   30
+ / \
+40   50
+         */
+
+        System.out.println("  \t10\n" +
+                           "   / \\\n" +
+                           "  20   30\n" +
+                           " / \\\n" +
+                           "40   50\n");
+
+
+
+
+  /*
+
+""   ""
+  \/
+         */
+    }
+}
+
+
+
+
+
---
+++ src/week02/Variables.java
@@ -0,0 +1,26 @@
+package week02;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(" ".repeat(9-i*2+1)+String.valueOf(i+1).repeat(i*2+1));
+        }
+
+
+
+/*
+  /*
+          1
+        222
+      33333
+    4444444
+  555555555
+
+     */
+
+
+
+
+    }
+}
